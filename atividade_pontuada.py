f6c8738727ee3f41a298e0fbfd039811f693d560
Diego Bras <150878045+GitPaulinho2@users.noreply.github.com>
2025-06-01 18:39:55 +0000

    Salvando

--- atividade_pontuada.py
+++ atividade_pontuada.py
@@ -4,19 +4,19 @@ os.system("cls || clear")
 from dataclasses import dataclass
 
 @dataclass
-class Biblioteca:
+class Biblioteca: # classe.
     livro: str
     autor: str
     data_publicado: str
     tema: str
     
-    def mostrar_dados(self):
+    def mostrar_dados(self): # mostra os dados do livro.
         print(f"Nome do Livro: {self.livro}")
         print(f"Nome do Autor: {self.autor}")
         print(f"Data públicada do livro: {self.data_publicado}")
         print(f"Tema do livro: {self.tema}")
 
-def cadastrar_livro(lista):
+def cadastrar_livro(lista): # cadastra os livros.
     print("|/| Biblioteca |/|\n")
     biblioteca = Biblioteca(
         livro = input("Digite o nome do livro desejado: "),
@@ -26,51 +26,51 @@ def cadastrar_livro(lista):
     )
     lista.append(biblioteca)
     
-def verificar_lista_vazia(lista):
+def verificar_lista_vazia(lista): # verificando se a lista está vazia.
     if not lista:
         print("A lista está vazia.")
         return True
     return False
 
-def mostrar_livros(lista):
+def mostrar_livros(lista): # mostra apenas os a variável dos livros.
     if verificar_lista_vazia(lista):
         return
     print("Lista de Livros\n")
     for livro in lista:
         print(f"- {livro.livro}")
        
-def buscar_livro(lista):
+def buscar_livro(lista): # procura o livro por meio de uma lista.
     if verificar_lista_vazia(lista):
         return
-    buscar_livro = input("Digite o nome do livro que deseja buscar: ")
+    buscar_livro = input("Digite o nome do livro que deseja buscar: ") # digita o nome do livro que aparece na lista.
     for biblioteca in lista:
         if biblioteca.livro == buscar_livro:
             os.system("cls || clear")
             print("Livro encontrado!\n")
-            biblioteca.mostrar_dados()
+            biblioteca.mostrar_dados() # mostra todos os dados do livro.
             print()
             return
         os.system("cls || clear")
         print("Livro não encontrado")
 
-def devolver_livro(lista):
-    if verificar_lista_vazia(lista):
+def devolver_livro(lista): # função que devolve o livro.
+    if verificar_lista_vazia(lista): # verifica se a lista está vazia.
         return
 
-    mostrar_livros(lista)
+    mostrar_livros(lista) # mostra todos os livros da lista.
 
     devolv_livro = input("Digite o nome do livro que deseja devolver: ")
     for livro in lista:
-        if livro.livro == devolv_livro:
+        if livro.livro == devolv_livro: # devolve o livro que deseja.
             lista.remove(livro)
         os.system("cls || clear")
         print(f"O livro {devolv_livro} foi devolvido com sucesso!")
         return
     print(f"O livro com o nome ({devolv_livro}) não foi encontrado.")
 
-lista = []
+lista = [] # lista
 
-while True: 
+while True: # repete o menu.
     os.system("cls || clear")
     print("""
  1- Cadastrar livro
@@ -80,9 +80,9 @@ while True:
  5- Sair
           """)
 
-    opcao = int(input("Digite uma das opções acima: "))
+    opcao = int(input("Digite uma das opções acima: ")) # opção para escolher no menu.
 
-    match opcao:
+    match opcao: # match case para o menu funcionar corretamente.
         case 1:
             os.system("cls || clear")
             cadastrar_livro(lista)
@@ -102,7 +102,7 @@ while True:
         case 5:
             os.system("cls || clear")
             print("Saindo...")
-            time.sleep(3)
+            time.sleep(3) # temporizador de 3 segundos pra dar um charme.
             break
         case _:
             print("Opção Inválida, Tente novamente.")
